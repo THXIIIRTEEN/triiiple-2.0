24574125bafa3ba58ba328fe264d9e7d40833714
THXIIIRTEEN <danilmatsarov@yandex.ru>
2025-06-30 23:02:23 +0500

    Update Notification.tsx

--- client/src/components/Notifications/Notification.tsx
+++ client/src/components/Notifications/Notification.tsx
@@ -17,7 +17,7 @@ const Notification: React.FC<NotificationProps> = ({notification, setNotificatio
     const { author, text, date, chatId, isRead, type } = notification;
     const { _id, username, tag } = author || notification;
     const [dateString, setDateString] = useState<string | null>(type !== 'friend' ? formateDate(date) : null); 
-
+    
     const router = useRouter();
 
     useEffect(() => {
@@ -34,7 +34,7 @@ const Notification: React.FC<NotificationProps> = ({notification, setNotificatio
 
     const handleDeleteNotification = async (e: React.MouseEvent<HTMLButtonElement>) => {
         e.stopPropagation();
-        setNotificationsArray((prev: IMessage[]) => {return prev.filter((msg) => msg._id !== _id)})
+        setNotificationsArray((prev: IMessage[]) => {return prev.filter((msg) => msg._id !== notification._id)})
         await deleteNotification(notification._id);
     }
 
